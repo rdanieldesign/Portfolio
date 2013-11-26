f88f71f8c4016b1582a21a72bb17fd509ce130c9
rdanieldesign@gmail.com <rdanieldesign@gmail.com>
2013-11-25 20:10:30 -0500

    Slide Navigation
    Switch to new slide navigation (arrows).

--- js/rd-ck.js
+++ js/rd-ck.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".closed").click(function(){$(this).addClass("open").removeClass("closed")})});
+$(document).ready(function(){$(".closed").click(function(){$(this).addClass("open").removeClass("closed");$(".open ul").css("overflow","hidden")});$(".exit").click(function(){$(".open").addClass("closed").removeClass("open")})});
--- js/rd.js
+++ js/rd.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 	$('.closed').click(function(){
 		$(this).addClass('open').removeClass('closed');
+		$(".open ul").css("overflow", "hidden");
+	});
+	$('.exit').click(function(){
+		$(".open").addClass('closed').removeClass('open');
 	});
 });
