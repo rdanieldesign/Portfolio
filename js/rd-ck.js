5461ceb883dd72f346f73cdc1c59ed21df81a014
rdanieldesign@gmail.com <rdanieldesign@gmail.com>
2013-11-25 22:22:16 -0500

    Add arrows, screw up positioning, but slides work.

--- js/rd-ck.js
+++ js/rd-ck.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".closed").click(function(){$(this).addClass("open").removeClass("closed");$(".open ul").css("overflow","hidden")});$(".exit").click(function(){$(".open").addClass("closed").removeClass("open")})});
+$(document).ready(function(){$(".closed").click(function(){$(this).addClass("open").removeClass("closed");$(".open ul").css("overflow","hidden");$(".slide-nav").css("display","block")});$("ul.slideshow").cycle({fx:"fade",pause:1,prev:".previous",next:".next"});$(".exit").click(function(){$(".open").addClass("closed").removeClass("open")})});
--- js/rd.js
+++ js/rd.js
@@ -2,7 +2,16 @@ $(document).ready(function(){
 	$('.closed').click(function(){
 		$(this).addClass('open').removeClass('closed');
 		$(".open ul").css("overflow", "hidden");
+		$(".slide-nav").css("display", "block");
 	});
+
+	$("ul.slideshow").cycle({
+	fx: 'fade',
+	pause: 1,
+	prev: '.previous',
+	next: '.next'
+	});
+
 	$('.exit').click(function(){
 		$(".open").addClass('closed').removeClass('open');
 	});
