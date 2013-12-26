18be6f9dadda0a9a96475618ec3a7a9ecd209948
rdanieldesign@gmail.com <rdanieldesign@gmail.com>
2013-12-26 12:08:19 -0500

    Allow mupltiple sliders on open and close

--- js/rd.js
+++ js/rd.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 	$('.closed').click(function(){
 		$('.open').addClass('closed').removeClass('open');
+		$('flexslider').removeClass('flexslider');
 		$(this).addClass('open').removeClass('closed');
-	    $('.flexslider').flexslider({
-	        animation: "slide",
-	        start: function(slider){
-	        $('body').removeClass('loading');
-	        }
-     
-    });
+		$('.open > .slider-hook').addClass('flexslider');
+    	$('.flexslider').flexslider({
+        animation: "slide",
+        start: function(slider){
+        $('body').removeClass('loading');
+        }
+	    });
 	});
 });
-
